8e1fec3099eaa763388fb57fce31fa632ab51477
Eric <460181594@qq.com>
2019-01-13 17:33:46 +0800

    代码优化

--- src/main/java/com/dx/ss/plugins/ptree/utils/NewTreeDataUtil.java
+++ src/main/java/com/dx/ss/plugins/ptree/utils/NewTreeDataUtil.java
@@ -1,25 +1,26 @@
 package com.dx.ss.plugins.ptree.utils;
 
+import com.dx.ss.plugins.ptree.model.TreeNode;
+
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.dx.ss.plugins.ptree.model.TreeNode;
-
 /**
  * 树形结构数据相关操作方法
+ *
  * @author Frank.Liu
  */
 public class NewTreeDataUtil {
 
     /**
      * 获取zTree树形结构数据
-     * @param result	返回的结构数据
-     * @param dataList	数据源，单个实体对象必须是继承自{@link TreeNode}
-     * @param isExpend	 初始是否展开节点    true-展开	false-收缩
-     * @param isHref 	是否点击打开链接
-     * @param isDrag 	是否可以拖动
-     * @return
+     *
+     * @param result   返回的结构数据
+     * @param dataList 数据源，单个实体对象必须是继承自{@link TreeNode}
+     * @param isExpend 初始是否展开节点    true-展开	false-收缩
+     * @param isHref   是否点击打开链接
+     * @param isDrag   是否可以拖动
      */
     public static String getZTreeJson(StringBuffer result, List<? extends TreeNode> dataList,
                                       boolean isExpend, boolean isHref, boolean isDrag) {
@@ -80,14 +81,9 @@ public class NewTreeDataUtil {
     }
 
     /**
-     * <p class="detail">
      * 功能：App树形结构组装
-     * </p>
+     *
      * @author liu.weihao
-     * @date 2016年4月18日 
-     * @param result
-     * @param dataList
-     * @return
      */
     public static String getTreeJson(StringBuilder result, List<? extends TreeNode> dataList) {
         if (result == null)
@@ -102,7 +98,7 @@ public class NewTreeDataUtil {
 
                 result.append(",\"name\":\"");
                 result.append(node.getName());
-                
+
                 result.append("\",\"level\":\"");
                 result.append(node.getNlevel());
 
@@ -120,7 +116,7 @@ public class NewTreeDataUtil {
 
                 result.append("\",\"level\":\"");
                 result.append(node.getNlevel());
-                
+
                 result.append("\",\"name\":\"");
                 result.append(node.getName());
 
@@ -129,14 +125,13 @@ public class NewTreeDataUtil {
         }
         return result.toString();
     }
-    
-    
-    
+
+
     /**
      * 判断某个节点是否有子节点
-     * @param dataList	数据源，单个实体对象必须是继承自{@link TreeNode}
-     * @param index	当前节点的索引值
-     * @return
+     *
+     * @param dataList 数据源，单个实体对象必须是继承自{@link TreeNode}
+     * @param index    当前节点的索引值
      */
     private static boolean hasChildMenu(List<?> dataList, int index) {
         boolean flag = false;
@@ -147,16 +142,16 @@ public class NewTreeDataUtil {
             int nextLevel = nextMenu.getNlevel(); //下个菜单层级
             if (nextLevel > curLevel) {
                 flag = true;
-            } 
+            }
         }
         return flag;
     }
 
     /**
      * 获取所有的子节点
-     * @param dataList	数据源，单个实体对象必须是继承自{@link TreeNode}
-     * @param index	当前节点的索引值
-     * @return
+     *
+     * @param dataList 数据源，单个实体对象必须是继承自{@link TreeNode}
+     * @param index    当前节点的索引值
      */
     private static List<TreeNode> getChildMenu(List<? extends TreeNode> dataList, int index) {
         List<TreeNode> children = new ArrayList<TreeNode>();
@@ -176,9 +171,9 @@ public class NewTreeDataUtil {
 
     /**
      * 将数据源转换成{@link TreeNode}
-     * @param dataList	数据源，单个实体对象必须是继承自{@link TreeNode}，TreeNode内的属性皆为树形结构数据共有的，name属性名可以在子类中定义
-     * @param methodName	通过反射调用该方法，设置name的值
-     * @return
+     *
+     * @param dataList   数据源，单个实体对象必须是继承自{@link TreeNode}，TreeNode内的属性皆为树形结构数据共有的，name属性名可以在子类中定义
+     * @param methodName 通过反射调用该方法，设置name的值
      */
     public static List<TreeNode> convertDataList(List<? extends TreeNode> dataList,
                                                  String methodName) {
--- src/main/java/com/dx/ss/plugins/ptree/utils/NodeCommon.java
+++ src/main/java/com/dx/ss/plugins/ptree/utils/NodeCommon.java
@@ -1,79 +1,58 @@
-/**
- * 
- */
 package com.dx.ss.plugins.ptree.utils;
 
-/**
- * @author panwuhai
- *
- */
 public class NodeCommon {
 
-	/**
-	 * 添加子节点层
-	 * 
-	 * @param fatherLevel
-	 * @return
-	 */
-	public static int addSubLevel(Integer fatherLevel) {
+    /**
+     * 添加子节点层
+     */
+    public static int addSubLevel(Integer fatherLevel) {
 
-		return fatherLevel.intValue() + 1;
-	}
+        return fatherLevel + 1;
+    }
 
-	/**
-	 * 新增子节点的左节点
-	 * 
-	 * @param fatherLeftNode
-	 *            父节点左节点
-	 * @return
-	 */
-	public static int addSubLeftNode(Integer fatherLeftNode) {
+    /**
+     * 新增子节点的左节点
+     *
+     * @param fatherLeftNode 父节点左节点
+     */
+    public static int addSubLeftNode(Integer fatherLeftNode) {
 
-		return fatherLeftNode.intValue() + 1;
-	}
+        return fatherLeftNode + 1;
+    }
 
-	/**
-	 * 新增子节点的右节点
-	 * 
-	 * @param fatherLeftNode
-	 *            父节点左节点
-	 * @return
-	 */
-	public static int addSubRightNode(Integer fatherLeftNode) {
+    /**
+     * 新增子节点的右节点
+     *
+     * @param fatherLeftNode 父节点左节点
+     */
+    public static int addSubRightNode(Integer fatherLeftNode) {
 
-		return fatherLeftNode.intValue() + 2;
-	}
+        return fatherLeftNode + 2;
+    }
 
-	/**
-	 * 添加平级节点层
-	 * 
-	 * @param beforeLevel
-	 * @return
-	 */
-	public static int addLevelLevel(Integer beforeLevel) {
+    /**
+     * 添加平级节点层
+     */
+    public static int addLevelLevel(Integer beforeLevel) {
 
-		return beforeLevel.intValue();
-	}
+        return beforeLevel;
+    }
 
-	/**
-	 * 新增平级节点的左节点
-	 * 
-	 * @param levelRightNode
-	 *            前平级节点的右节点
-	 * @return
-	 */
-	public static int addLevelLeftNode(Integer levelRightNode) {
-		return levelRightNode.intValue() + 1;
-	}
+    /**
+     * 新增平级节点的左节点
+     *
+     * @param levelRightNode 前平级节点的右节点
+     */
+    public static int addLevelLeftNode(Integer levelRightNode) {
+        return levelRightNode + 1;
+    }
 
-	/**
-	 * 新增平级节点的右节点
-	 * 
-	 * @param levelRightNode
-	 *            前平级节点的右节点
-	 * @return
-	 */
-	public static int addLevelRightNode(Integer levelRightNode) {
-		return levelRightNode.intValue() + 2;
-	}
+    /**
+     * 新增平级节点的右节点
+     *
+     * @param levelRightNode 前平级节点的右节点
+     */
+    public static int addLevelRightNode(Integer levelRightNode) {
+        return levelRightNode + 2;
+    }
 }
